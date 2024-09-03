191e42406c9c7af4884701fb6d2e782e2a55bf6a
iskoulou <johnskoul56@gmail.com>
2024-09-03 12:21:24 +0300

    first commit

---
+++ app/src/main/java/com/example/geofence2021091/Point.java
@@ -0,0 +1,13 @@
+package com.example.geofence2021091;
+
+public class Point {
+
+    public Integer sessionId;
+
+    public String type;
+
+    public Double longitude;
+
+    public Double latitude;
+
+}
