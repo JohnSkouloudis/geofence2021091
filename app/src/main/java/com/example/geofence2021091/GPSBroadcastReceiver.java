6e5ec25d2eddd59e61ae4809f04fcf48197ff487
JohnSkouloudis <153203600+JohnSkouloudis@users.noreply.github.com>
2024-09-19 18:52:55 +0300

    Update GPSBroadcastReceiver.java

--- app/src/main/java/com/example/geofence2021091/GPSBroadcastReceiver.java
+++ app/src/main/java/com/example/geofence2021091/GPSBroadcastReceiver.java
@@ -15,18 +15,16 @@ public class GPSBroadcastReceiver extends BroadcastReceiver {
 
             if (isGpsEnabled) {
                 // GPS is enabled
-                // Perform actions for GPS being enabled
-//                Toast.makeText(context, "GPS is enabled", Toast.LENGTH_SHORT).show();
-//                int a = 4;
+                 //Toast.makeText(context, "GPS is enabled", Toast.LENGTH_SHORT).show();
+
                 Intent intent2 = new Intent();
                 intent2.setClass(context.getApplicationContext(), LocationService.class);
                 context.startService(intent2);
-//                Log.d("CUSTOM MESSAGE","GPS is enabled");
+                //Log.d("CUSTOM MESSAGE","GPS is enabled");
             } else {
                 // GPS is disabled
-                // Perform actions for GPS being disabled
-//                Toast.makeText(context, "GPS is disabled", Toast.LENGTH_SHORT).show();
-//                int b = 3;
+                
+//                Toast.makeText(context, "GPS is disabled", Toast.LENGTH_SHORT).show();               
 //                Log.d("CUSTOM MESSAGE","GPS is disabled");
                 Intent intent3 = new Intent(context.getApplicationContext(), LocationService.class);
                 context.stopService(intent3);
